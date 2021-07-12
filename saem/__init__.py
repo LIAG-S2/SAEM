004864b117c6304884aa73808508430196ae44d6
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-07-12 08:28:39 +0200

    module ready

---
+++ saem/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Refraction seismics or first arrival traveltime calculations."""
+
+
+from .saem import CSEMData
+# from .plotting import showSounding
+
+
+__all__ = [
+    'CSEMData',
+]
