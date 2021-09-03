22553e0363ed3d36f66542e388acd60c59d1f921
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-09-03 09:55:19 +0200

    tweak Giesen data

--- data/giesen/procData.py
+++ data/giesen/procData.py
@@ -10,15 +10,24 @@ import pygimli as pg
 # %% import transmitter (better by kmlread)
 txpos = np.array([[559497.46, 5784467.953],
                   [559026.532, 5784301.022]]).T
-self = CSEMData(datafile="data_f*.mat", txPos=txpos, txalt=70)
+self = CSEMData(datafile="data_f2.mat", txPos=txpos, txalt=70)
 self.DATAX *= -1  # why?
-self.filter(fmin=50, fmax=24000)
+# self.filter(fmin=50, fmax=20000)
+# self.filter(f=12000)
+# self.filter(f=7000)
+# self.filter(f=5000)
+print(self.f)
 print(self)
-# self.generateDataPDF()
 # self.showData(nf=1)
-# self.showField("alt", background="BKG")
-self.cmp[0] = 1
+# %%
+self.setPos(5)
+self.cmp[0] = 0
 self.cmp[1] = 1
+self.showSounding()
+# %%
+sdffs
+# self.showField("alt", background="BKG")
+# self.generateDataPDF()
 self.invertSounding(nrx=20)
 # resp=self.inv1d.response.array()
 # %%
