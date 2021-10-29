6ddd76dd9a370e3b50b3946fd077d3dd004bf46e
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-10-29 09:46:18 +0200

    update Giesen data and scripts

--- data/giesen/procDataNew.py
+++ data/giesen/procDataNew.py
@@ -9,39 +9,30 @@ txpos = np.array([[559497.46, 5784467.953],
 self = CSEMData(datafile="data_f*.mat", txPos=txpos, txalt=70)
 self.DATAX *= -1  # why?
 self.showField(self.line)
-self.cmp[1] = 1
-self.cmp[2] = 0
-self.showLineData(line=7)
-dfsfsd
-
+# %%
+self.line[414:421] = 0
 self.filter(fmin=100, fmax=20000)
 self.filter(f=12000)
 self.filter(f=7000)
-# self.filter(f=5000)
-# self.filter(f=900)
-print(self.f)
-print(self)
-# self.showData(nf=1)
-# self.generateDataPDF()
-# self.showField("alt", background="BKG")
 # %%
-self.setPos(320, show=True)  # middle
-# self.setPos(333, show=True)  # close to transmitter
-# self.setPos(310, show=True)
-# self.cmp = [1, 1, 1]
-# self.showSounding(amphi=False)
+self.setPos(440, show=True)  # middle
 # %%
-# self.depth = np.hstack((0, np.cumsum(10**np.linspace(0.8, 2, 15))))
+self.createDepthVector(rho=30)  # , nl=12)
+self.depth *= 0.7
 print(self.depth)
 # %%
 self.cmp[0] = 0  # no x (Tx)
 self.cmp[1] = 1
 self.cmp[2] = 0
-# %
-ikw = dict(absError=0.001, relError=0.03, lam=3)
+ikw = dict(absError=0.0015, relError=0.04, lam=20)
 self.invertSounding(**ikw)
+dfdg
 # %%
-line = 6
+line = 7
 self.invertLine(line=line, **ikw)
 ax = self.showSection(cMin=3, cMax=200)
 ax.figure.savefig(f"line{line}-resultY.pdf", bbox_inches="tight")
+# %%
+dfsfsdf
+# %%
+self.invertLine(**ikw)
