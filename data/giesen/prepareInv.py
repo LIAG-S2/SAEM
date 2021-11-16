2a31d8a955435c8345f5c1d0c37d7720ecf5327b
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-11-16 09:14:19 +0100

    strip Tx-near and very far Rx and export all 2D lines and 3D

--- data/giesen/prepareInv.py
+++ data/giesen/prepareInv.py
@@ -11,25 +11,35 @@ self = CSEMData(datafile="data_f*.mat", txPos=txgeo, txalt=70,
 self.DATAY *= -1  # why?
 self.cmp = [1, 1, 1]
 # %%
-self.line[414:421] = 0  # remove tx-near
 self.filter(fmin=100, fmax=20000)
 self.filter(f=12000)
 self.filter(f=7000)
 # %%
 self.createDepthVector(rho=30)  # , nl=12)
 self.depth *= 0.7
+# print(self.depth)
 self.model = np.ones_like(self.depth) * 25
-print(self.depth)
 # %%
 line = 7
-npos = 440
-self.setPos(npos)
-self.showLineData(line, alim=[-3, 0])
 # %%
 self.rotatePositions(line=line)
-self.showLineData(line, alim=[-3, 0])
+# self.showLineData(line, alim=[-3, 0])
 # %%
-self.cmp = [1, 0, 0]
+self.cmp = [1, 0, 1]
 self.showLineData(line, alim=[-2, 0], plim=[-90, 0])
 # %%
+# %% compute distance to transmitter
+self.line[self.rx < 100] = 0
+self.line[self.rx > 700] = 0
+self.showField("line")
+# self.showPos()
+self.setPos(0)
 self.saveData(line="all")
+self.filter(nInd=np.nonzero(self.line > 0)[0])
+self.filter(nInd=np.nonzero(self.line < 15)[0])
+self.showPos()
+# %%
+self.filter(nInd=np.arange(0, len(self.rx), 4))
+self.basename += "take4"
+self.saveData()
+self.showPos()
