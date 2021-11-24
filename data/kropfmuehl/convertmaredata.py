d2431d95aa0b191dfb5d309631f234dbfdd97ec3
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-11-24 21:16:59 +0100

    add saving topo

--- data/kropfmuehl/convertmaredata.py
+++ data/kropfmuehl/convertmaredata.py
@@ -1,3 +1,4 @@
+import numpy as np
 from saem.maredata import Mare2dEMData
 
 self = Mare2dEMData("P5.emdata")
@@ -13,3 +14,7 @@ self.generateDataPDF()
 self.saveData()
 for tx in [1, 2]:
     self.saveData(tx=tx)
+# %%
+ind = np.argsort(self.rxpos[:, 0])
+topo = self.rxpos[ind, ::2]
+np.savetxt("topo.txt", topo)
