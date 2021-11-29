377cbe17577b71ac1eeb3f355a07a912bd759705
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-11-29 16:03:15 +0100

    add Gosetal profile

---
+++ data/goslar/convertmaredata.py
@@ -0,0 +1,15 @@
+import numpy as np
+from saem.maredata import Mare2dEMData
+
+self = Mare2dEMData("GOS_raw_inversion_ByBz.emdata")
+print(self)
+# %%
+self.chooseF(every=2)
+print(self)
+print(self.f)
+self.basename += "f2"
+self.generateDataPDF()
+# %%
+self.saveData(topo=1)
+for tx in [1, 2, 3]:
+    self.saveData(tx=tx, topo=1)
