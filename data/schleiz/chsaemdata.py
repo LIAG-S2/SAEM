0486ab2170963b015f821950e9401c2364316609
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-12-03 16:34:48 +0100

    add schleiz topo and edit global position information for LOTEM and SAEM data

--- data/schleiz/chsaemdata.py
+++ data/schleiz/chsaemdata.py
@@ -2,9 +2,16 @@ import numpy as np
 from saem.maredata import Mare2dEMData
 
 self = Mare2dEMData("L07_bybz_edited.emdata")
-self.showPositions()
-self.saveData()
 # %%
-for tx in range(5):
-    part1 = self.getPart(tx=tx+1)
-    print(np.unique(part1.DATA["Freq"]))
+ax = self.showPositions(True)
+ax.figure.savefig(self.basename+"-pos.pdf", bbox_inches="tight", dpi=300)
+# %%
+lotem = Mare2dEMData("Ball.emdata")
+ax = lotem.showPositions(True)
+ax.figure.savefig(self.basename+"-pos.pdf", bbox_inches="tight", dpi=300)
+# self.saveData()
+# %%
+# for tx in range(5):
+#     part1 = self.getPart(tx=tx+1)
+#     print(np.unique(part1.DATA["Freq"]))
+#
