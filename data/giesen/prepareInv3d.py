cb50333e26edd083d0164be0fe718b0a7d2b50ba
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2021-12-14 13:50:42 +0100

    prepare Giesen for 3D inversion (no rotation, just shift)

---
+++ data/giesen/prepareInv3d.py
@@ -0,0 +1,34 @@
+import numpy as np
+from saem import CSEMData
+
+
+# %% import transmitter (better by kmlread)
+txgeo = np.array([[559497.46, 5784467.953],
+                  [559026.532, 5784301.022]]).T
+self = CSEMData(datafile="data_f*.mat", txPos=txgeo, txalt=70,
+                basename="giesen")
+self.DATAY *= -1  # why?
+self.filter(fmin=50, fmax=20000)
+self.filter(f=12000)
+# %%
+rot = CSEMData(datafile="data_f*.mat", txPos=txgeo, txalt=70,
+               basename="giesen")
+rot.rotatePositions()
+rot.showPos()
+self.rx -= rot.origin[0]
+self.ry -= rot.origin[1]
+self.tx -= rot.origin[0]
+self.ty -= rot.origin[1]
+# %%
+n = np.nonzero((rot.rx > 100)*(rot.rx < 700))[0]
+# %%
+self.filter(nInd=n)
+self.filter(nInd=np.arange(0, len(self.rx), 4))
+self.showPos()
+self.basename += "-take4org"
+# %%
+self.saveData(cmp=[1, 1, 1])
+for i in range(3):
+    cmp = [0, 0, 0]
+    cmp[i] = 1
+    self.saveData(cmp=cmp)
