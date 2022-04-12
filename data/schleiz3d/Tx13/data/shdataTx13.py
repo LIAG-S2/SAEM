9023013c23ff2f07d0e44430748d006f41850320
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2022-04-12 11:36:33 +0200

    load Tx13 data

--- data/schleiz3d/Tx13/data/shdataTx13.py
+++ data/schleiz3d/Tx13/data/shdataTx13.py
@@ -1,9 +1,11 @@
 import numpy as np
-from saem import CSEMSurvey
+from saem import CSEMSurvey, CSEMData
 
 tx13 = np.loadtxt("Tx13.pos")
 marefile = "Schleiz_Tx13_L01_L14_BxByBz_47deg_estmationerrors.emdata"
 self = CSEMSurvey(marefile, txs=[tx13], flipxy=True)
 self.showData()
-# self.basename=self.patches[0].basename
-self.patches[0].generateDataPDF(marefile.replace(".emdata", ".pdf"))
+p = self.patches[0]
+# p.generateDataPDF(marefile.replace(".emdata", ".pdf"))
+p.saveData()
+bla=CSEMData("patch1BxByBz.npz")  # check load since restructure
