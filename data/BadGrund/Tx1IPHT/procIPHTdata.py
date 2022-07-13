5336ad70eaf9c87fdb314e1d2b901e514d3c1e68
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2022-07-13 11:10:42 +0200

    add IPHT Tx2 proc script

---
+++ data/BadGrund/Tx1IPHT/procIPHTdata.py
@@ -0,0 +1,32 @@
+import numpy as np
+from saem import CSEMData
+
+
+txpos = np.genfromtxt("../Tx2.pos").T[:, ::-1]
+# %% read in the old data and take every second data point (12->24m)
+self = CSEMData(datafile="data/*.mat", txPos=txpos) # , txalt=ground)
+dx = np.sqrt(np.diff(self.rx)**2+np.diff(self.ry)**2)
+print(np.median(dx))
+self.filter(every=5)
+self.radius = 60
+self.showField("line")
+# %%
+self.filter(fmin=12, fmax=1100)
+self.filter(fInd=np.arange(0, len(self.f), 2))
+# self.filter(f=10.)
+# %%
+self.showLineData(line=5)
+# %%
+self.rotate()
+# %%
+self.filter(minTxDist=300, maxTxDist=3000)
+# %%
+self.showData(line=5)
+# %%
+self.cmp = [1, 0, 1]
+self.saveData("Tx2IPHT_BxBz")
+self.filter(every=2)
+self.saveData("Tx2IPHT_BxBzE2")
+self.filter(every=2)
+self.saveData("Tx2IPHT_BxBzE4")
+self.saveData("Tx2IPHT_BzE4", cmp=[0, 0, 1])
