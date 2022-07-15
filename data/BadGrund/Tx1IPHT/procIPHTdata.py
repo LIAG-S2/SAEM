34a51ecf4ab74bd8f97364a0e7296a4529951dca
Thomas Günther <Thomas.Guenther@leibniz-liag.de>
2022-07-15 09:19:10 +0200

    finish BadGrund/Tx1IPHT/procIPHTdata.py

--- data/BadGrund/Tx1IPHT/procIPHTdata.py
+++ data/BadGrund/Tx1IPHT/procIPHTdata.py
@@ -3,35 +3,37 @@ from saem import CSEMData
 
 
 txpos = np.genfromtxt("../Tx2.pos").T[:, ::-1]
-# %% read in the old data and take every second data point (12->24m)
-self = CSEMData(datafile="data/*.mat", txPos=txpos) # , txalt=ground)
+self = CSEMData(datafile="data/*.mat", txPos=txpos)
 dx = np.sqrt(np.diff(self.rx)**2+np.diff(self.ry)**2)
-print(np.median(dx))
+print("Median Rx distance: ", np.median(dx))
 self.filter(every=5)
-self.radius = 60
-self.showField("line")
+self.radius = 60  # make plot similar to WWU bird
+# self.showField("line")
 # %%
 self.filter(fmin=12, fmax=1100)
-self.filter(fInd=np.arange(0, len(self.f), 2))
-# self.filter(f=10.)
+self.filter(fInd=np.arange(0, len(self.f), 2))  # every second
 # %%
 self.showLineData(line=5)
 # %%
-self.rotate()
-# %%
-self.filter(minTxDist=300, maxTxDist=3000)
+self.filter(minTxDist=500, maxTxDist=3000)
 self.deactivateNoisyData(rErr=0.5)
-self.estimateError()
-self.deactivateNoisyData(rErr=0.5, aErr=0.01)
+self.estimateError()  # 5%+1pV/A
+self.deactivateNoisyData(rErr=0.5)
 # %%
 self.showData(line=5)
 # %%
-txdir = -1
-self.cmp = [1, 0, 1]
-self.saveData("Tx2IPHT_BxBz", txdir=txdir)
+self.setOrigin([580000, 5740000])
+self.showField("line")
+# self.rotate(ang=0)  #
+# %%
+self.cmp = [0, 1, 1]
+self.saveData("Tx2IPHT_ByBz")
 self.filter(every=2)
-self.saveData("Tx2IPHT_BxBzE2", txdir=txdir)
+self.saveData("Tx2IPHT_ByBzE2")
 self.filter(every=2)
-self.saveData("Tx2IPHT_BxBzE4", txdir=txdir)
-self.saveData("Tx2IPHT_BzE4", cmp=[0, 0, 1], txdir=txdir)
-self.saveData("Tx2IPHT_BxE4", cmp=[1, 0, 0], txdir=txdir)
+self.saveData("Tx2IPHT_ByBzE4")
+# %%
+self.saveData("Tx2IPHT_BxE4", cmp=[1, 0, 0])
+self.saveData("Tx2IPHT_ByE4", cmp=[0, 1, 0])
+self.saveData("Tx2IPHT_BzE4", cmp=[0, 0, 1])
+self.saveData("Tx2IPHT_BxByBzE4", cmp=[1, 1, 1])
