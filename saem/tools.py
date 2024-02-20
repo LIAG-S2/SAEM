e61e3644c5c40fa6e39c9ed21a21f94f706cf2bc
Thomas Günther <halbmy@pygimli.org>
2024-02-20 21:36:12 +0100

    REF make polygone robust to dimension

--- saem/tools.py
+++ saem/tools.py
@@ -21,9 +21,9 @@ def is_point_inside_polygon(x, y, polygon):
     """Determine whether a point is inside a polygon."""
     n = len(polygon)
     inside = False
-    p1x, p1y = polygon[0]
+    p1x, p1y, *_ = polygon[0]
     for i in range(n + 1):
-        p2x, p2y = polygon[i % n]
+        p2x, p2y, *_ = polygon[i % n]
         if y > min(p1y, p2y) and y <= max(p1y, p2y) and x <= max(p1x, p2x):
             if p1y != p2y:
                 xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
