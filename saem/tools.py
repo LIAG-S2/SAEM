eae4990265e93d0cb404fb9ebcf89e1f3310dd69
Thomas Günther <halbmy@pygimli.org>
2024-02-20 21:18:10 +0100

    ADD is_point_inside_polygon tool function

--- saem/tools.py
+++ saem/tools.py
@@ -17,6 +17,23 @@ def distToTx(rx, ry, tx, ty):
     return np.sqrt(dist2)
 
 
+def is_point_inside_polygon(x, y, polygon):
+    """Determine whether a point is inside a polygon."""
+    n = len(polygon)
+    inside = False
+    p1x, p1y = polygon[0]
+    for i in range(n + 1):
+        p2x, p2y = polygon[i % n]
+        if y > min(p1y, p2y) and y <= max(p1y, p2y) and x <= max(p1x, p2x):
+            if p1y != p2y:
+                xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                if p1x == p2x or x <= xinters:
+                    inside = not inside
+
+        p1x, p1y = p2x, p2y
+
+    return inside
+
 def detectLinesAlongAxis(rx, ry, axis='x'):
     """Alternative - Split data in lines for line-wise processing."""
 
