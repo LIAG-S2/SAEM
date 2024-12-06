41c81376647f868e01f064577416f94062ca81b4
Moouuzi <raphael.rochlitz@leibniz-liag.de>
2024-12-06 10:34:39 +0100

    reverted utm coordinate detection of WWU mat type import, changed in July, seems still not to work as supposed, these changes are still included as comments for comparison

--- saem/csem.py
+++ saem/csem.py
@@ -276,17 +276,32 @@ class CSEMData(EMData):
 
         self.MAT = MAT
         self.f = np.round(100.0 / np.squeeze(MAT["periods"])) / 100.
+        
+        # # fix does not work as supposed 
+        # if "lla" in MAT.dtype.names:
+        #     # import utm
+        #     # self.rx, self.ry, *_ = utm.from_latlon(*MAT["lla"][:2])
+        #     self.rx, self.ry = self.utm(*MAT["lla"][1::-1])
+        #     self.rz = MAT["lla"][2]
+        # else:
+        #     self.ry, self.rx = MAT["xy"]  # can be wrong
+        #     if "topo" in MAT.dtype.names:
+        #         self.rz = MAT["topo"][0]
+        #     else:
+        #         raise Exception("Could not determine altitude!")
+        
+        # using old version from June 2024
+        self.ry, self.rx = MAT["xy"]  # can be wrong
+        # import utm
+        # self.rx, self.ry, *_ = utm.from_latlon(*MAT["lla"][:2])
+        # self.rx, self.ry = self.utm(*MAT["lla"][1::-1])
+
+        # if "topo" in MAT.dtype.names:
+        #     self.rz = MAT["topo"][0]
         if "lla" in MAT.dtype.names:
-            # import utm
-            # self.rx, self.ry, *_ = utm.from_latlon(*MAT["lla"][:2])
-            self.rx, self.ry = self.utm(*MAT["lla"][1::-1])
             self.rz = MAT["lla"][2]
         else:
-            self.ry, self.rx = MAT["xy"]  # can be wrong
-            if "topo" in MAT.dtype.names:
-                self.rz = MAT["topo"][0]
-            else:
-                raise Exception("Could not determine altitude!")
+            raise Exception("Could not determine altitude!")
 
         if "line" in MAT.dtype.names:
             self.line = MAT["line"]
